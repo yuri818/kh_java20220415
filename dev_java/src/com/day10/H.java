9525b911452c0b5499bf78f30e0da0c1c0b18e81
yuri818 <2yooree98@naver.com>
2022-05-02 15:19:46 +0900

    20220502 수업

--- dev_java/src/com/day10/H.java
+++ dev_java/src/com/day10/H.java
@@ -3,8 +3,11 @@ package com.day10;
 public class H {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 0 , j = 1;
+		if((i++ == 0) | (j++ == 2)) { //i=1,j=2 //0==0 or 1==2
+			i = 42;
+		} //만약 ||라면 i=42,j=1
+		System.out.println("i="+ i + ",j= " + j);
 	}
 
 }
